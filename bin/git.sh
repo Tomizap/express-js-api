d788bc37af39bf05388bdf6754efb812115b5025
Tom ZAPICO <zaptom.pro@gmail.com>
2024-08-01 13:32:57 +0200

    push

---
+++ bin/git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TEMPLATE_REPO="https://github.com/Tomizap/express-js-api.git"
+TEMPLATE_BRANCH="main"
+
+git remote add template $TEMPLATE_REPO
+git fetch template
+git merge template/$TEMPLATE_BRANCH
+git push origin main
